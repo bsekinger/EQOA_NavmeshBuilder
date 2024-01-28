8835937f6f81a7e6e723ac1307538f92c502a43c
bsekinger <bsekinger@gmail.com>
2024-01-27 19:34:42 -0500

    initial Commit

--- EQOA_NavmeshBuilder/Settings.h
+++ EQOA_NavmeshBuilder/Settings.h
@@ -5,24 +5,25 @@
 class Settings
 {
 public:
-    std::string Path = "D:\\ESF-file-format\\Tunaria_FixOrigin\\";
+    std::string Path = "D:\\ESF - file - format\\Tunaria_FixOrigin\\";
     std::string meshFile;
-    int minFile = 166;
-    int maxFile = 174;
-    float cellSize = 0.25f;
-    float cellHeight = 0.25f;
+    int minFile = 87;
+    int maxFile = 87;
+
+    float cellSize = 0.1f;
+    float cellHeight = 0.1f;
     float agentHeight = 2.0f;
     float agentRadius = 0.6f;
     float agentMaxClimb = 0.9f;
     float agentMaxSlope = 60.0f;
     float regionMinSize = 0.25f;
     float regionMergeSize = 20.0f;
-    float edgeMaxLen = 12.0f;
-    float edgeMaxError = 1.3f;
+    float edgeMaxLen = 6.0f;
+    float edgeMaxError = 1.0f;
     float vertsPerPoly = 6.0f;
-    float detailSampleDist = 6.0f;
-    float detailSampleMaxError = 1.0f;
-    float tileSize = 400.0f;
+    float detailSampleDist = 3.0f;
+    float detailSampleMaxError = 0.5f;
+    float tileSize = 40.0f;
     int partitionType = SAMPLE_PARTITION_WATERSHED;
 
     static Settings& getInstance() {
